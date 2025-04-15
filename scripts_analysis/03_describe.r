e6e7a7bc230c221085cb4aa0f2bb9f340e6c2209
Yanwen Wang <yanwenwang@u.nus.edu>
2025-04-15 22:47:17 +0800

    Enhance descriptive analysis script with statistical summaries and tests
    This commit updates the `03_describe.r` script to include rounded mean and standard deviation calculations for various variables, improving readability. Additionally, it incorporates t-tests and chi-squared tests for comparative analysis between paired and unpaired samples, enhancing the statistical rigor of the descriptive analysis.

--- scripts_analysis/03_describe.r
+++ scripts_analysis/03_describe.r
@@ -28,48 +28,54 @@ sample_df %>%
 # 2 Income and housework roles --------------------------------------------
 
 tabyl(sample_df, combined_role) %>%
+  arrange(desc(n)) %>%
   adorn_pct_formatting(2)
 
 tabyl(sample_df, income_role) %>%
+  arrange(desc(n)) %>%
   adorn_pct_formatting(2)
 
 tabyl(sample_df, housework_role) %>%
+  arrange(desc(n)) %>%
   adorn_pct_formatting(2)
 
 # 3 Other variables --------------------------------------------------------
 
 # Life satisfaction
-mean(sample_df$lsat)
-sd(sample_df$lsat)
+mean(sample_df$lsat) %>% round(3)
+sd(sample_df$lsat) %>% round(3)
 
-mean(sample_df$lsat_sp)
-sd(sample_df$lsat_sp)
+mean(sample_df$lsat_sp) %>% round(3)
+sd(sample_df$lsat_sp) %>% round(3)
+
+t.test(sample_df$lsat, sample_df$lsat_sp)
 
 # Income
-mean(sample_df$income, na.rm = TRUE)
-sd(sample_df$income, na.rm = TRUE)
+mean(sample_df$income, na.rm = TRUE) %>% round(3)
+sd(sample_df$income, na.rm = TRUE) %>% round(3)
 
-mean(sample_df$income_sp, na.rm = TRUE)
-sd(sample_df$income_sp, na.rm = TRUE)
+mean(sample_df$income_sp, na.rm = TRUE) %>% round(3)
+sd(sample_df$income_sp, na.rm = TRUE) %>% round(3)
+
+t.test(sample_df$income, sample_df$income_sp)
 
 # Housework hour
-mean(sample_df$housework_hour)
-sd(sample_df$housework_hour)
+mean(sample_df$housework_hour) %>% round(3)
+sd(sample_df$housework_hour) %>% round(3)
 
-mean(sample_df$housework_hour_sp)
-sd(sample_df$housework_hour_sp)
+mean(sample_df$housework_hour_sp) %>% round(3)
+sd(sample_df$housework_hour_sp) %>% round(3)
+
+t.test(sample_df$housework_hour, sample_df$housework_hour_sp)
 
 # Age
-mean(sample_df$age)
-sd(sample_df$age)
+mean(sample_df$age) %>% round(3)
+sd(sample_df$age) %>% round(3)
 
-# Age squared
-mean(sample_df$age_sp, na.rm = TRUE)
-sd(sample_df$age_sp, na.rm = TRUE)
+mean(sample_df$age_sp, na.rm = TRUE) %>% round(3)
+sd(sample_df$age_sp, na.rm = TRUE) %>% round(3)
 
-# Cohabitation
-tabyl(sample_df, marital) %>%
-  adorn_pct_formatting(2)
+t.test(sample_df$age, sample_df$age_sp)
 
 # Education
 tabyl(sample_df, educ) %>%
@@ -78,12 +84,16 @@ tabyl(sample_df, educ) %>%
 tabyl(sample_df, educ_sp) %>%
   adorn_pct_formatting(2)
 
+chisq.test(sample_df$educ, sample_df$educ_sp)
+
 # Hukou
 tabyl(sample_df, hukou) %>%
   adorn_pct_formatting(2)
 
 tabyl(sample_df, hukou_sp) %>%
   adorn_pct_formatting(2)
+
+chisq.test(sample_df$hukou, sample_df$hukou_sp)
 
 # Migrant
 tabyl(sample_df, migrant) %>%
@@ -92,13 +102,16 @@ tabyl(sample_df, migrant) %>%
 tabyl(sample_df, migrant_sp) %>%
   adorn_pct_formatting(2)
 
+chisq.test(sample_df$migrant, sample_df$migrant_sp)
+
 # Chronic
 tabyl(sample_df, chronic) %>%
   adorn_pct_formatting(2)
 
-# Chronic spouse
 tabyl(sample_df, chronic_sp) %>%
   adorn_pct_formatting(2)
+
+chisq.test(sample_df$chronic, sample_df$chronic_sp)
 
 # Number of children
 tabyl(sample_df, n_children) %>%
